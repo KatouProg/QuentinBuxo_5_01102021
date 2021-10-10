c2e51a8d3df49d1df1b382e3721ed780255c1ff0
Quentin BUXO <katouprog@gmail.com>
2021-10-10 22:58:27 +0900

    JS_Commentaires explicatifs(perso)

--- front/js/index.js
+++ front/js/index.js
@@ -6,9 +6,9 @@ function main() {
 
 // Récupération des fiches-article depuis l'API --> http://localhost:3000/api/products
 
-function getArticles() {                            // J'envoie une requête HTTP
+function getArticles() {                                                            // ---------- J'envoie une requête HTTP ----------
     fetch("http://localhost:3000/api/products")
-    .then(function (res) {                          // Je récupère le résultat de la requête
+    .then(function (res) {                                                          // ---------- Je récupère le résultat de la requête ----------
         return res.json();
     })
     .catch((error) => {
@@ -19,33 +19,33 @@ function getArticles() {                            // J'envoie une requête HTT
     // Répartition des données de l'API dans le DOM
 
     .then(function (resultAPI) {                    
-        const articles = resultAPI;                 // Je crée ma boucle pour afficher mes articles
+        const articles = resultAPI;                 
         console.table(articles);
-        for (let article in articles) {
+        for (let article in articles) {                                             // ---------- Je crée ma boucle pour afficher mes articles ----------
 
             // Création de "a" (lien)
             let productLink = document.createElement("a");
-            document.querySelector(".items").appendChild(productLink);              // Je crée mon "a"
-            productLink.href = `product.html?id=${resultAPI[article]._id}`;         // Redirect vers la page article (./product.html?id=...)
+            document.querySelector(".items").appendChild(productLink);              // ---------- Je crée mon "a" ----------
+            productLink.href = `product.html?id=${resultAPI[article]._id}`;         // ---------- Redirect vers la page article (./product.html?id=...) ----------
 
-            // Création de "article"
+            // Création de "article" (section)
             let productArticle = document.createElement("article");
-            productLink.appendChild(productArticle);                                // Je crée mon "article"
+            productLink.appendChild(productArticle);                                // ---------- Je crée mon "article" ----------
 
             // Création de "img" (image)
             let productImg = document.createElement("img");
-            productArticle.appendChild(productImg);                                 // Je crée mon "img"         
+            productArticle.appendChild(productImg);                                 // ---------- Je crée mon "img" ----------         
             productImg.src = resultAPI[article].imageUrl;
             productImg.alt = resultAPI[article].altTxt;
 
             // Création de "h3" (titre)
             let productName = document.createElement("h3");
-            productArticle.appendChild(productName);                                // Je crée mon "h3"
+            productArticle.appendChild(productName);                                // ---------- Je crée mon "h3" ----------
             productName.innerHTML = resultAPI[article].name;
 
             // Création de "p" (description)
             let productDescription = document.createElement("p");
-            productArticle.appendChild(productDescription);                         // Je crée mon "p"
+            productArticle.appendChild(productDescription);                         // ---------- Je crée mon "p" ----------
             productDescription.innerHTML = resultAPI[article].description;
         }
       });

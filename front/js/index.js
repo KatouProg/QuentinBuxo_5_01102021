b7d5a11c78b1cd639d9f688e845adf43ab9d3300
Quentin BUXO <katouprog@gmail.com>
2021-10-10 22:52:28 +0900

    JS_Creation de la page index.js + implémentation des produits_Commentaires

--- front/js/index.js
+++ front/js/index.js
@@ -23,27 +23,27 @@ function getArticles() {                            // J'envoie une requête HTT
         console.table(articles);
         for (let article in articles) {
 
-            // Implémentation de "a" (lien)
+            // Création de "a" (lien)
             let productLink = document.createElement("a");
             document.querySelector(".items").appendChild(productLink);              // Je crée mon "a"
             productLink.href = `product.html?id=${resultAPI[article]._id}`;         // Redirect vers la page article (./product.html?id=...)
 
-            // Implémentation de "article"
+            // Création de "article"
             let productArticle = document.createElement("article");
             productLink.appendChild(productArticle);                                // Je crée mon "article"
 
-            // Implémentation de "img" (image)
+            // Création de "img" (image)
             let productImg = document.createElement("img");
             productArticle.appendChild(productImg);                                 // Je crée mon "img"         
             productImg.src = resultAPI[article].imageUrl;
             productImg.alt = resultAPI[article].altTxt;
 
-            // Implémentation de "h3" (titre)
+            // Création de "h3" (titre)
             let productName = document.createElement("h3");
             productArticle.appendChild(productName);                                // Je crée mon "h3"
             productName.innerHTML = resultAPI[article].name;
 
-            // Implémentation de "p" (description)
+            // Création de "p" (description)
             let productDescription = document.createElement("p");
             productArticle.appendChild(productDescription);                         // Je crée mon "p"
             productDescription.innerHTML = resultAPI[article].description;

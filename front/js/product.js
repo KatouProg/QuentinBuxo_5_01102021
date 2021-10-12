89e5a0d59213d397678a17884a74b31992851e1d
Quentin BUXO <katouprog@gmail.com>
2021-10-12 13:10:02 +0900

    JS_Création product.js + récupération articles depuis API

--- front/js/product.js
+++ front/js/product.js
@@ -6,3 +6,21 @@ var idProduct = url.searchParams.get("id");
 console.log(idProduct);
 
 
+const colorPicked = document.querySelector("#colors");
+const quantityPicked = document.querySelector("#quantity");
+
+
+// Récupération des articles depuis l'API
+
+getArticle();
+
+function getArticle() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+    .then(function (res) {
+        return res.json();
+    })
+    .catch((error) => {
+        console.log("Accès à l'API impossible... Déso frérot !!!");
+    })
+
+

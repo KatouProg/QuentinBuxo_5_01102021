45220e5b68508e5ccffc94085a8e3abb1d204c2c
Quentin BUXO <katouprog@gmail.com>
2021-10-17 11:03:23 +0900

    JS_cart.js --> Si panier vide

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,3 +5,8 @@ let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 console.table(produitLocalStorage);
 const positionEmptyCart = document.querySelector("#cart__items");
 
+// --> Le panier est vide
+if (produitLocalStorage === null || produitLocalStorage == 0) {
+    const emptyCart = `<p>Votre panier est vide</p>`;
+    positionEmptyCart.innerHTML = emptyCart;
+} 
